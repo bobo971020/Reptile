99bd99ae2f056bc70e9df19a6ef061dc5f482896
liubo <982781738@qq.com>
2018-12-26 23:32:42 +0800

    add Requests.py

--- Requests_demo.py
+++ Requests_demo.py
@@ -9,9 +9,25 @@ Time       : 2018-12-26 23:11
 """
 
 import requests
-r = requests.get("https://baidu.com")
-print(type(r))
-print(r.status_code)
-print(type(r.text))
-print(r.text)
-print(r.cookies)
+# r = requests.get("https://baidu.com")
+# print(type(r))      # 输出返回类型
+# print(r.status_code)    # 打印状态码
+# print(type(r.text))     # 打印文本类型
+# print(r.text)       # 输出文本
+# print(r.cookies)    # 输出cookie
+#
+# print(requests.post("http://httpbin.org/post"))
+# print(requests.put("http://httpbin.org/put"))
+# print(requests.delete("http://httpbin.org/delete"))
+# print(requests.head("http://httpbin.org/get"))
+# print(requests.options("http://httpbin.org/get"))
+
+# r = requests.get('http://httpbin.org/get')
+# print(r.text)
+#
+# data ={
+#     'name': "germey",
+#     'age': 22
+# }
+# r = requests.get("http://httpbin.org/get", params=data)     # 添加参数
+# print(r.text)
